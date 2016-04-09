2dc19eb28000495014f0a038930d04c36c54219b
Michael Antis <antismichael@gmail.com>
2016-04-09 14:45:30 -0400

    remove second destructor

--- dbl_link.h
+++ dbl_link.h
@@ -34,6 +34,5 @@ class DoublyLinkedList {
         DoublyLinkedList(const Node<type> &copy);
         ~DoublyLinkedList();
         DoublyLinkedList operator=(const Node<type> &);
-        ~DoublyLinkedList();
 
 };
