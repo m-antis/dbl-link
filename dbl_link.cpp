745ec42b27df5eaf2beaec3d310ebfdc4c1adacc
Michael Antis <antismichael@gmail.com>
2016-04-12 19:22:02 -0400

    add destroyList(), printList() functions

--- dbl_link.cpp
+++ dbl_link.cpp
@@ -6,12 +6,14 @@ void DoublyLinkedList<type>::prepend(const type &node) {
     if(!isEmpty()) {
         first->prev = node;
         node->next = first;
+        length++;
         cout << "\nNode successfully prepended"; // for testing purposes
     }
     else {
         first = node;
         last = node;
         last->next = NULL;
+        length++;
     }
 }
 
@@ -21,23 +23,56 @@ void DoublyLinkedList<type>::append(const type &node) {
     if(!isEmpty()) {
         node->prev = last;
         node->next = NULL;
+        length++;
         cout << "\nNode successfully appended"; // for testing purposes
-
     }
     else {
         first = node;
         last = node;
         last->next = NULL;
+        length++;
     }
-
 }
 
-void append(const type &);
-void destroyList();
-void destroyNode(const type &);
+template<class type>
+void DoublyLinkedList<type>::destroyList() {
+    first = last = NULL;
+    length = 0;
+}
+
+template <class type>
+void DoublyLinkedList<type>::printList() {
+    Node<type> *p;
+    p = first;
+    if(!isEmpty()) {
+        while(p) {
+            cout << p->datum;
+            p = p->next;
+        }
+    }
+    else {
+        cout << "\nList is empty. Nothing to print."
+    }
+}
+
+// template<class type>
+// void DoublyLinkedList<type>::destroyNode(const type &item) {
+//
+//     Node<type> *p;
+//     p = first;
+//
+//     while(p) {
+//         if(p->datum == item) {
+//
+//         }
+//         p = p->next;
+//     }
+// }
+
+
+
+
+
 bool isEmpty() const;
 int length() const;
 void printList() const;
-
-
-DoublyLinkedList::
